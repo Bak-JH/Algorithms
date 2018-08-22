363d537f7a64b6c1e46c2d6a9a5c5fd8cd39be7c
Junhyeong Bak <bakjh.6280@gmail.com>
2018-08-22 19:13:14 +0900

    CodeWars Algorithm problem

---
+++ Roman_numberals_encoder.cpp
@@ -0,0 +1,19 @@
+﻿string solution(int number) {
+	string result;
+	int numbers[13] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	string romanNumbers[13] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+	for (int numberIdx  = 0; numberIdx  < 13; ++numberIdx)
+	{
+		if (number / numbers[numberIdx] > 0)
+		{
+			for (int j  = 0; j  < number  / numbers[numberIdx]; ++j) {
+				result  += romanNumbers[numberIdx];
+			}
+
+			number  %= numbers[numberIdx];
+		}
+	}
+
+	return result;
+}
